090e85ceaa9cb4eea8b0f0b6688fdf377e184f3f
miran248 <miran248+github@cflow.io>
2018-08-09 13:51:07 +0200

    feat(linkedlist): Add Serializable decorator to LinkedList

--- packages/linkedlist/src/Node.js
+++ packages/linkedlist/src/Node.js
@@ -14,23 +14,29 @@ export default class Node {
 
   constructor() {
     decorate(this, [
-      new Linkable(),
-      new Replacable(),
-      new Reusable((data) => {
-        this.data = data;
-        data.owner = this;
+      new Linkable,
+      new Replacable,
+      new Reusable(
+        (data) => {
+          this.data = data;
+          data.owner = this;
 
-        get(this.data, Shareable).retain();
-      }),
-      new Recyclable(() => {
-        const link = get(this, Linkable);
+          get(this.data, Shareable).retain();
+        }
+      ),
+      new Recyclable(
+        () => {
+          const link = get(this, Linkable);
 
-        if(link.parent)
-          link.parent.detach(this);
+          if(link.parent)
+            link.parent.detach(this);
 
-        this.data = get(this.data, Shareable).release();
-      }),
-      new Serializable(() => get(this.data, Serializable).serialize()),
+          this.data = get(this.data, Shareable).release();
+        }
+      ),
+      new Serializable(
+        () => get(this.data, Serializable).serialize()
+      ),
     ]);
   }
 
